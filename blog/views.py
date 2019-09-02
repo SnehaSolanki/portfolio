8ce4bc3712e6e6960086bf87f670b79006955545
SnehaSolanki <SnehaSolanki@users.noreply.github.com>
2019-09-02 14:39:11 +0530

    added new static file for image

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
+
+def projects(request):
+    projects = Blog.objects
+    return render(request, 'blog/projects.html', {'projects':projects})
+
+def detail(request,blog_id):
+    pdetail=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/pdetail.html', {'pdetail':pdetail})
